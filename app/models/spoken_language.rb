a7bc1084a59eb3ba6677ef45368c02889e29c338
Tiago Cardoso <tiago@restorm.com>
2013-03-16 02:27:35 +0100

    previously missing spoken language changes

--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -1,4 +1,10 @@
 # -*- encoding : utf-8 -*-
-class SpokenLanguage
-  # To change this template use File | Settings | File Templates.
+class SpokenLanguage < ActiveRecord::Base
+  belongs_to :user
+
+  has_enumerated :language
+  has_enumerated :language_level
+
+  validates :user, :language_code, :language_level_code, presence: true
+  validates :language_code, uniqueness: { scope: :user_id }
 end
--- spec/factories/spoken_languages.rb
+++ spec/factories/spoken_languages.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password { "password" }
+  factory :spoken_language do
+    association :user, factory: :user
+    language_code { Language.codes.sample }
+    language_level_code { LanguageLevel.codes.sample }
   end
 end
