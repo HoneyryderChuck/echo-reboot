9846568f8141d529b01f08d057e8805626b4956c
Tiago Cardoso <tiago@restorm.com>
2013-03-16 03:37:30 +0100

    added clean up rule for database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
     DatabaseCleaner.start
   end
   config.after(:each) do
