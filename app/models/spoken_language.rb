1dd87b1b297ac36cf8f24a89af4a4ebfab6471f0
Tiago Cardoso <tiago@restorm.com>
2013-03-31 01:46:21 +0100

    added convenience scopes to spoken language (necessary to establish minimum language requirements)

--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -7,4 +7,10 @@ class SpokenLanguage < ActiveRecord::Base
 
   validates :user, :language_code, :language_level_code, presence: true
   validates :language_code, uniqueness: { scope: :user_id }
+
+  LanguageLevel.codes.each do |code|
+    scope :"#{code}", proc { where(language_level_code: code ) }
+    valuable_codes = LanguageLevel.codes.slice(0, LanguageLevel.codes.index(code)+1)
+    scope :"at_least_#{code}", proc { where(language_level_code: valuable_codes) }
+  end
 end
--- spec/models/spoken_language_spec.rb
+++ spec/models/spoken_language_spec.rb
@@ -14,4 +14,42 @@ describe SpokenLanguage do
       should validate_uniqueness_of(:language_code).scoped_to(:user_id)
     }
   end
+
+  describe "scopes" do
+    before(:all) do
+      @language1 = FactoryGirl.create(:spoken_language, language_level_code: "mother_tongue")
+      @language2 = FactoryGirl.create(:spoken_language, language_level_code: "advanced")
+      @language3 = FactoryGirl.create(:spoken_language, language_level_code: "intermediate")
+      @language4 = FactoryGirl.create(:spoken_language, language_level_code: "basic")
+    end
+    after(:all) do
+      DatabaseCleaner.clean
+    end
+    it ".mother_tongue" do
+      SpokenLanguage.mother_tongue.should == [@language1]
+    end
+    it ".advanced" do
+      SpokenLanguage.advanced.should == [@language2]
+    end
+    it ".intermediate" do
+      SpokenLanguage.intermediate.should == [@language3]
+    end
+    it ".basic" do
+      SpokenLanguage.basic.should == [@language4]
+    end
+    describe ".at_least_" do
+      it "mother_tongue" do
+        SpokenLanguage.at_least_mother_tongue.should == [@language1]
+      end
+      it "advanced" do
+        SpokenLanguage.at_least_advanced.should == [@language1, @language2]
+      end
+      it "intermediate" do
+        SpokenLanguage.at_least_intermediate.should == [@language1, @language2, @language3]
+      end
+      it "basic" do
+        SpokenLanguage.at_least_basic.should == [@language1, @language2, @language3, @language4]
+      end
+    end
+  end
 end
