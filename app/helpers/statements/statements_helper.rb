f9d2eb05b153d30eeb65d7fd2b6d7782683c24ca
Tiago Cardoso <tiago@restorm.com>
2013-04-01 17:19:56 +0200

    added some missing translations to have some less headaches.

--- app/helpers/statements/statements_helper.rb
+++ app/helpers/statements/statements_helper.rb
@@ -2,7 +2,7 @@ module Statements::StatementsHelper
 
   def supporters_number(statement)
     count = statement.new_record? ? 1 : statement.echo.supporter_count
-    I18n.t("discuss.statements.echo_indicator", :supporter_count => count)
+    I18n.t("discuss.statements.echo_indicator", count: count)
   end
 
 end
